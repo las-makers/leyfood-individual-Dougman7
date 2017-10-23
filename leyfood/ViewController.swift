02089a2c2f473e16d9958bdb03dbd2246823232a
Elliott Hebert <elliott.hebert.pro@gmail.com>
2017-10-23 17:00:42 +0200

    Initial Commit

---
+++ leyfood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  leyfood
+//
+//  Created by ELLIOTT HEBERT on 23.10.17.
+//  Copyright © 2017 LAS Makers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
